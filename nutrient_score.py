e7fcaa44611af5e86bd743568ac95c5bb7d5de12
Yangbaechu <abab_1212@naver.com>
2024-04-10 07:20:53 +0000

    Feat - food recommendation with dummy data

--- nutrient_score.py
+++ nutrient_score.py
@@ -8,16 +8,17 @@ from openai import OpenAI
 
 client = OpenAI(
     # This is the default and can be omitted
-    api_key=os.environ.get("OPENAI_API_KEY"),
+    api_key=os.environ["OPENAI_API_KEY"]
 )
 
-def get_embedding(text, model="text-embedding-3-small"):
+def get_embedding(text, model):
     text = text.replace("\n", " ")
     response = client.embeddings.create(
         input=[text],
-        model=model
+        model=model,
+        dimensions=256
     )
-    return response.data[0].embedding[0]
+    return response.data[0].embedding
 
 def normalize_distance(df, columns, max_values):
     normalized_df = df.copy()
@@ -108,7 +109,7 @@ def make_combined_data():
 def read_combined_data():
     combined_data = pd.read_csv('./ingredient/combined_data.csv')
 
-    print(combined_data.head())
+    # print(combined_data.head())
 
     return combined_data
 
@@ -141,18 +142,18 @@ max_values = {
 normalized_df = normalize_distance(df, columns_to_convert, max_values)
 
 # 예시 값 (단백질, 에너지, 지방, 탄수화물)
-protein = 13
-energy = 600
-fat = 32
+protein = 5
+energy = 400
+fat = 22
 carbohydrate = 123
 
 # 유사한 데이터 추출
-similar_data = find_similar_food(df, normalized_df, protein, energy, fat, carbohydrate, max_values, top_n=2)
+similar_data = find_similar_food(df, normalized_df, protein, energy, fat, carbohydrate, max_values, top_n=10)
 
 #나트륨, 콜레스테롤, 포화지방산 데이터 전처리
-similar_data['나트륨(㎎)'] = similar_data['나트륨(㎎)'].replace('-', 0)
-similar_data['콜레스테롤(㎎)'] = similar_data['콜레스테롤(㎎)'].replace('-', 0)
-similar_data['총 포화 지방산(g)'] = similar_data['총 포화 지방산(g)'].replace('-', 0)
+similar_data['나트륨(㎎)'] = similar_data['나트륨(㎎)'].replace('-', '0')
+similar_data['콜레스테롤(㎎)'] = similar_data['콜레스테롤(㎎)'].replace('-', '0')
+similar_data['총 포화 지방산(g)'] = similar_data['총 포화 지방산(g)'].replace('-', '0')
 
 similar_data['나트륨(㎎)'] = similar_data['나트륨(㎎)'].str.replace(',', '').astype(float)
 similar_data['콜레스테롤(㎎)'] = similar_data['콜레스테롤(㎎)'].str.replace(',', '').astype(float)
@@ -161,10 +162,21 @@ similar_data['총 포화 지방산(g)'] = similar_data['총 포화 지방산(g)'
 #유해성분(나트륨, 콜레스테롤, 포화지방산)이 낮은 순으로 점수 매기기(높을수록 좋음)
 similar_data['nutrient_score'] = similar_data['sim_score'] - similar_data['나트륨(㎎)']/10 - similar_data['콜레스테롤(㎎)']/100 - similar_data['총 포화 지방산(g)']/10
 
-
 # nutrient_score가 높은 순으로 정렬
 similar_foods = similar_data.sort_values(by='nutrient_score', ascending=False)
 similar_foods['food_embedding'] = similar_foods['식품명'].apply(lambda x: get_embedding(x, model='text-embedding-3-large'))
-#print(df['식품명'])
-#print(get_embedding(df['식품명'], model='text-embedding-3-large'))
-print(similar_foods[['식품명', 'food_embedding', 'sim_score', 'nutrient_score', '나트륨(㎎)', '콜레스테롤(㎎)', '총 포화 지방산(g)']])
+print(similar_foods[['식품명', 'sim_score', 'nutrient_score', '나트륨(㎎)', '콜레스테롤(㎎)', '총 포화 지방산(g)']])
+#print(similar_foods['food_embedding'].iloc[0])
+
+#사용자 embedding 추출
+
+#예시 값(dimension 256인 랜덤 값)
+user_embedding = np.random.rand(256)
+
+#사용자 embedding과 음식 embedding의 내적값 계산
+similar_foods['user_embedding'] = [user_embedding] * len(similar_foods)
+similar_foods['dot'] = similar_foods.apply(lambda x: np.dot(x['food_embedding'], x['user_embedding']), axis=1)
+
+#내적값이 높은 순으로 정렬
+similar_foods = similar_foods.sort_values(by='dot', ascending=False)
+print(similar_foods[['식품명', 'food_embedding', 'sim_score', 'nutrient_score', 'dot', '나트륨(㎎)', '콜레스테롤(㎎)', '총 포화 지방산(g)']])
